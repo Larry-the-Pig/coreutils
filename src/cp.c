cc3c031168f3e93db26ef74d374449cbaf1ecdd5
Larry the Pig <larrythepig123@gmail.com>
2024-03-31 21:56:41 -0600

    proper error handling in copy command

--- src/cp.c
+++ src/cp.c
@@ -12,16 +12,20 @@ int main(int argc, char** argv) {
     if (argc < 3)
     return 1;
     
+    int old_errno;
+
     int fd_from = open(argv[1], O_RDONLY);
 
     if (fd_from < 0) {
-        // error handle
+        perror(PROGRAM_NAME);
+        return 1;
     }
 
     int fd_to = open(argv[2], O_CREAT | O_WRONLY | O_EXCL, 0666);
 
     if (fd_to < 0) {
-        // error handle
+        //fd_to = -1;
+        goto error;
     }
 
     char buffer[BUFFER_SIZE];
@@ -34,14 +38,43 @@ int main(int argc, char** argv) {
 
         nwritten = write(fd_to, buffer, nread);
         if (nwritten == -1) {
-            // error handle
+            goto error;
         }
         out_ptr += nwritten;
     }
 
     if (nread == -1) {
-        // error handle
+        goto error;
+    }
+
+    if (close(fd_from) < 0) {
+        fd_from = -1;
+        goto error;
+    }
+
+    if (close(fd_to) < 0) {
+        fd_to = -1;
+        goto error;
     }
 
     return 0;
+
+    // at this point fd_open was successful
+    error:
+        old_errno = errno;
+        if (fd_from != -1) {
+            if (close(fd_from) < 0) {
+                perror(PROGRAM_NAME);
+            }
+        }
+
+        if (fd_to != -1) {
+            if (close(fd_to) < 0) {
+                perror(PROGRAM_NAME);
+            }
+        }
+
+        errno = old_errno;
+        perror(PROGRAM_NAME);
+        return 1;
 }
