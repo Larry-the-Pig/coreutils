061408005903850fecd225692ad3c7f2846dab85
Larry the Pig <larrythepig123@gmail.com>
2024-04-04 18:25:22 -0600

    make cat posix compliant

--- src/cat.c
+++ src/cat.c
@@ -1,33 +1,67 @@
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 #define PROGRAM_NAME "cat"
 
 int main(int argc, char** argv) {
     if (argc < 2)
     return 0;
+
+    int old_errno;
     
-    for (int i = 1; i < argc; i++) {
-        FILE* file = fopen(argv[i], "r");
-        if (file == NULL) {
-            perror(PROGRAM_NAME);
-            return 1;
+    int i = 1;
+
+    if (strcmp(argv[1], "-u") == 0)
+    i = 2;
+
+    for (; i < argc; i++) {
+
+        int fd = open(argv[i], O_RDONLY);
+        if (fd == -1) {
+            goto error;
         }
 
         char buffer[256];
 
-        // fgets WILL write a NULL terminator
-        while (fgets(buffer, 256, file)) {
-            printf("%s", buffer);
+        ssize_t nread;
+        
+        while (nread = read(fd, buffer, 256), nread > 0) {
+            if (write(STDOUT_FILENO, buffer, nread) == -1) {
+                goto error;
+            }
         }
 
-        int close_status = fclose(file);
-        if (close_status != 0) {
+        if (nread == -1) {
+            goto error;
+        }
+
+        if (close(fd) == -1) {
+            fd = -1;
+
+            goto error;
+        }
+
+        // advance before label
+        continue;
+
+        error:
+            old_errno = errno;
+            if (fd != -1) {
+                if (close(fd) < 0) {
+                    perror(PROGRAM_NAME);
+                }
+            }
+
+            errno = old_errno;
             perror(PROGRAM_NAME);
             return 1;
-        }
     }
 
     return 0;
+
 }
